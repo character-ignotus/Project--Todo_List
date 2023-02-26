fdabe2b69f76d38c7e99dc4f9f623b21d9268e89
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-02-26 23:08:51 +0200

    Add a factory function to create basic todo items

---
+++ src/todos.js
@@ -0,0 +1,6 @@
+function todosCreator(title, description, date, priorityChecker, isChecked) {
+    const priority = (() => priorityChecker === 1 ? 'low' : 'high')();
+    const checked = (() => isChecked ? true : false)();
+
+    return {title, description, date, priority, checked};
+}
