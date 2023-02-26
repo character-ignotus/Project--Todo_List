69043e303f29b338f792f2ed91cb14877f4b62b3
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-02-26 22:45:45 +0200

    Set up Webpack Dev Environment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('Am i working?')
+function generate() {
+    const el = document.createElement('div');
+    el.style.width = '500px';
+    el.style.height = '500px';
+    el.style.backgroundColor = 'red';
+
+    return el;
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    open: true,
+    hot: true
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+      filename: 'index.html',
+      template: 'src/template.html'
+    })
+  ]
+};
