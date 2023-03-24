10e46252991c999cd717f94991ccd1dfb1cf1d7d
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-03-24 21:42:20 +0200

    Populate project view

--- src/dom.js
+++ src/dom.js
@@ -10,7 +10,30 @@ function closeProjectsModal() {
         modal.close();
 };
 
-export {openProjectsModal, closeProjectsModal};
+function createProjectElement(project) {
+    const projectCard = document.createElement('div');
+    const projecOpentBtn = document.createElement('button');
+    const projecCloseBtn = document.createElement('button');
+
+    projecOpentBtn.textContent = `${project.projectName}`;
+    projecCloseBtn.textContent = `X`;
+
+    projectCard.classList.add('project');
+
+    projectCard.appendChild(projecOpentBtn);
+    projectCard.appendChild(projecCloseBtn);
+
+    return projectCard;
+}
+
+function populateProjectsView() {
+    let projectName = document.querySelector('#project-name').value;
+    createProject(projectName);
+    const projects = getProjects();
+    return createProjectElement(projects[projects.length - 1]);
+}
+
+export {openProjectsModal, closeProjectsModal, populateProjectsView};
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './styles/style.css'
 import { createProject, createTodos, delProject, delTodo, loger } from "./controller";
-import { openProjectsModal, closeProjectsModal } from "./dom"
+import { openProjectsModal, closeProjectsModal, populateProjectsView } from "./dom"
 
 document.querySelector('#add-project').addEventListener('click', openProjectsModal);
 document.querySelector('.close-project-modal').addEventListener('click', closeProjectsModal);
+
+document.querySelector('.submit-project').addEventListener('click', () => {
+    document.querySelector('#projects').appendChild(populateProjectsView());
+    window.loger();
+});
 
 window.loger = loger;
 window.createProject = createProject;
