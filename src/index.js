275274df2fcf6d20f0dd76d2a97d5c9684f3f83f
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-05-06 00:02:21 +0300

    Check todo element submission for empty values

--- src/index.js
+++ src/index.js
@@ -12,12 +12,23 @@ document.querySelector('#add-todo').addEventListener('click', openTodosModal);
 document.querySelector('.close-todos-modal').addEventListener('click', closeTodosModal);
 // Opening & closing modal for todos //
 
+function checkValues(e) {
+    const inputs = Array.from(e.target.parentElement.firstElementChild.nextElementSibling.querySelectorAll('input'));
+
+    for(let i = 0; i < inputs.length; i++) {
+        if(inputs[i].value == '') {
+            return true;
+        };
+    };
+}
+
 document.querySelector('.submit-project').addEventListener('click', () => {
     document.querySelector('#projects').appendChild(populateProjectsView());
     window.loger();
 });
 
-document.querySelector('.submit-todo').addEventListener('click', () => {
+document.querySelector('.submit-todo').addEventListener('click', (e) => {
+    if(checkValues(e)) return;
     document.querySelector('#todos-view').appendChild(populateCurrentProject());
     window.loger();
 });
